53cc130abf7fc4dec9a3dfd0364bf9aabb433fd1
Mateor13 <mateo.torres@epn.edu.ec>
2024-07-12 12:08:23 -0500

    Buscar

--- src/Insertar.java
+++ src/Insertar.java
@@ -2,10 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Insertar extends JFrame {
     private JTextField cod;
--- src/buscar.java
+++ src/buscar.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
-public class buscar {
-    private JTextField textField1;
+public class buscar extends JFrame {
+    private JTextField co;
     private JButton buscarBtn;
     private JLabel cod1;
     private JLabel nm;
@@ -13,12 +15,57 @@ public class buscar {
     private JLabel cat;
     private JButton RGBtn;
     private JButton LoginBtn;
+    private JPanel Bsc;
 
     public buscar() {
+        setTitle("Buscar Producto");
+        setContentPane(Bsc);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(500, 250));
+        pack();
+        setVisible(true);
+        setLocationRelativeTo(null);
         buscarBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                String url = "jdbc:mysql://localhost:3306/productos_cp";
+                String user = "root";
+                String pass = "123456";
+                String querry = "select * from producto where codigo_producto = '"+co.getText()+"'";
+                try (Connection con = DriverManager.getConnection(url, user, pass)){
+                    Statement st = con.createStatement();
+                    ResultSet rs = st.executeQuery(querry);
+                    if (rs.next()){
+                        cod1.setText("Código de producto: "+ rs.getString("codigo_producto"));
+                        nm.setText("Nombre: "+rs.getString("nombre"));
+                        des.setText("Descripción: "+rs.getString("descripcion"));
+                        pre.setText("Precio: "+rs.getString("precio"));
+                        cant.setText("Cantidad: "+rs.getString("cantidad"));
+                        cat.setText("Categoria: "+rs.getString("categoria"));
+                        setPreferredSize(new Dimension(500, 400));
+                        pack();
+                        setLocationRelativeTo(null);
+                    }else{
+                        cod1.setText("Usuario no encontrado");
+                    }
+                }catch (SQLException e1){
+                    e1.printStackTrace();
+                    cod1.setText("Error en la Base de datos");
+                }
+            }
+        });
+        RGBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Insertar();
+                dispose();
+            }
+        });
+        LoginBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new login();
+                dispose();
             }
         });
     }
